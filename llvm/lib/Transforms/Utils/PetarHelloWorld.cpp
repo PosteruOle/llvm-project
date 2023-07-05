64f782218d1e0ca00d1cc7edb68322dfaeb415b9
Petar Tesic <petar.tesic@syrmia.com>
2023-07-05 17:57:17 +0200

    Preparing cttz count for testing by eliminating extra output!

--- llvm/lib/Transforms/Utils/PetarHelloWorld.cpp
+++ llvm/lib/Transforms/Utils/PetarHelloWorld.cpp
@@ -1,32 +1,26 @@
-#include "llvm/Transforms/Utils/PetarHelloWorld.h"
+#include "llvm/Transforms/Utils/PetarCountCttz.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicInst.h"
-#include "llvm/Support/Regex.h"
 
 using namespace llvm;
-int count_cttz_intrinsic_appearance=0;
+int countCttzIntrinsicAppearance=0;
 
-PreservedAnalyses PetarHelloWorldPass::run(Function &F, FunctionAnalysisManager &AM) {
-  Regex llvmcttzRegex("^llvm.cttz.*");
+PreservedAnalyses PetarCountCttzPass::run(Function &F, FunctionAnalysisManager &AM) {
+
   errs() << "Function name: " << F.getName() << "\n";
-  errs() << "\n-------------------------------\n";
-  errs() << "Instructions within this function!\n";
+  //errs() << "\n-------------------------------\n";
+  //errs() << "Instructions within this function!\n";
   for(BasicBlock& BB: F){
     for(Instruction& I: BB){
-       errs()<< I.getName() << "\n";
+       //errs()<< I.getName() << "\n";
+       IntrinsicInst *II = dyn_cast<IntrinsicInst>(&I);
        
-       if(CallBase::classof(&I)){
-          CallBase* callBase = (CallBase*) &I;
-          StringRef functionName = callBase->getCalledFunction()->getName();
-          if(llvmcttzRegex.match(functionName)){
-            errs() << "Look what we have found -> " << functionName << "\n";
-            count_cttz_intrinsic_appearance++;
-          }  
-       }     
+       if(II && (II->getIntrinsicID() == Intrinsic::cttz))
+         countCttzIntrinsicAppearance++;
     }
   }
-  errs() << "\n-------------------------------\n";
-  errs() << "Total number of cttz intrinsic appearances is equal to: " << count_cttz_intrinsic_appearance << ".\n";
+  //errs() << "\n-------------------------------\n";
+  errs() << "Total number of cttz intrinsic appearances is equal to: " << countCttzIntrinsicAppearance << ".\n";
 
   return PreservedAnalyses::all();
 }
