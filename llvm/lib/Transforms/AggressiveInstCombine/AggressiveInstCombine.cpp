70ef6196b6fe3a3e6a6837656b1a05b381af1c70
PosteruOle <posteru.ole@gmail.com>
2023-11-01 22:16:04 +0100

    Added code generator for a optimal version of a CRC algorithm!

--- llvm/lib/Transforms/AggressiveInstCombine/AggressiveInstCombine.cpp
+++ llvm/lib/Transforms/AggressiveInstCombine/AggressiveInstCombine.cpp
@@ -943,12 +943,13 @@ static bool tryToRecognizeCRC32(Instruction &I){
   Value *argument2=LIfinal->getFunction()->getArg(1);
   Type* ArgType1=argument1->getType();
   Type* ArgType2=argument2->getType();
-  
   IRBuilder<> B(LIfinal);
-  auto CRC8 = B.CreateIntrinsic(Intrinsic::crc8, {}, {argument1, argument2});
-  // New insertion!
+  
+  //Something to remember because it's very important!
+  //auto CRC8 = B.CreateIntrinsic(Intrinsic::crc8, {}, {argument1, argument2});
+  //LIfinal->replaceAllUsesWith(CRC8);
+  //LIfinal->removeFromParent();
 
-  LIfinal->replaceAllUsesWith(CRC8);
   BasicBlock *bb_help10=RIfinal->getParent()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode();
   BasicBlock *bb_help9=RIfinal->getParent()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode();
   BasicBlock *bb_help8=RIfinal->getParent()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode()->getPrevNode();
@@ -960,6 +961,114 @@ static bool tryToRecognizeCRC32(Instruction &I){
   BasicBlock *bb_help2=RIfinal->getParent()->getPrevNode()->getPrevNode();
   BasicBlock *bb_help1=RIfinal->getParent()->getPrevNode();
   DeleteDeadBlocks({bb_help1, bb_help2, bb_help3, bb_help4, bb_help5, bb_help6, bb_help7, bb_help8, bb_help9, bb_help10});
+  
+  //Another very important thing!
+  //Instruction *CRC8cast=dyn_cast<Instruction>(CRC8);
+  //IRBuilder<> Builder(CRC8cast);
+
+  IRBuilder<> Builder(LIfinal);
+  Function *F=LIfinal->getFunction();
+  LIfinal->getParent()->setName("entry");
+  AllocaInst *AI1=Builder.CreateAlloca(ArgType1, nullptr, "data.addr");
+  AllocaInst *AI2=Builder.CreateAlloca(ArgType2, nullptr, "_crc.addr");
+  AllocaInst *AI3=Builder.CreateAlloca(ArgType1, nullptr, "i");
+  AllocaInst *AI4=Builder.CreateAlloca(ArgType1, nullptr, "x16");
+  AllocaInst *AI5=Builder.CreateAlloca(ArgType1, nullptr, "carry");
+  AllocaInst *AI6=Builder.CreateAlloca(ArgType1, nullptr, "crc");
+  StoreInst *SI1=Builder.CreateStore(argument1, AI1);
+  StoreInst *SI2=Builder.CreateStore(argument2, AI2);
+  StoreInst *SI3=Builder.CreateStore(Builder.getInt8(0), AI3);
+  StoreInst *SI4=Builder.CreateStore(Builder.getInt8(0), AI4);  
+  StoreInst *SI5=Builder.CreateStore(Builder.getInt8(0), AI5);
+  LoadInst *LI1=Builder.CreateLoad(ArgType2, AI2, "");
+  Value *ZI1=Builder.CreateZExt(LI1, Builder.getInt64Ty(), "conv");
+  StoreInst *SI6=Builder.CreateStore(ZI1, AI6);
+  LoadInst *LI2=Builder.CreateLoad(Builder.getInt8Ty(), AI1, "");
+  Value *ZI2=Builder.CreateZExt(LI2, Builder.getInt64Ty(), "conv1");
+  LoadInst *LI3=Builder.CreateLoad(Builder.getInt64Ty(), AI6, "");
+  Value *XOR1=Builder.CreateXor(LI3, ZI2, "xor");
+  StoreInst *SI7=Builder.CreateStore(XOR1, AI6);
+  StoreInst *SI8=Builder.CreateStore(Builder.getInt8(0), AI3);
+  
+  BasicBlock* ForEndBB = BasicBlock::Create(F->getContext(), "for.end", F, nullptr);
+  Instruction *I1=LIfinal;
+  Instruction *I2=LIfinal->getNextNode(); 
+  I1->removeFromParent();
+  I2->removeFromParent();
+  I1->insertInto(ForEndBB, ForEndBB->end());
+  I2->insertInto(ForEndBB, ForEndBB->end());
+
+  //IRBuilder<> Builder1(SI8->getParent(), SI8->getParent()->end());
+  //BranchInst *BI1=Builder1.CreateBr(ForCondBB);
+
+  //I think we can start this way!
+  BasicBlock* ForCondBB = BasicBlock::Create(F->getContext(), "for.cond", F, ForEndBB);
+  IRBuilder<> ForCondBuilder(ForCondBB, ForCondBB->end()); 
+  BasicBlock* ForBodyBB = BasicBlock::Create(F->getContext(), "for.body", F, ForEndBB);
+  IRBuilder<> ForBodyBuilder(ForBodyBB, ForBodyBB->end());
+  BasicBlock* ForIncBB = BasicBlock::Create(F->getContext(), "for.inc", F, ForEndBB);
+  IRBuilder<> ForIncBuilder(ForIncBB, ForIncBB->end());  
+
+  IRBuilder<> Builder1(SI8->getParent(), SI8->getParent()->end());
+  BranchInst *BI1=Builder1.CreateBr(ForCondBB);
+
+  //Everything is okay with creation of "for.cond" basic block! 
+  LoadInst *LI4=ForCondBuilder.CreateLoad(ForCondBuilder.getInt8Ty(), AI6);
+  Value *ZI3=ForCondBuilder.CreateZExt(LI4, ForCondBuilder.getInt32Ty(), "conv2");
+  Value *ICI1=ForCondBuilder.CreateICmp(llvm::CmpInst::ICMP_SLT, ZI3, ForCondBuilder.getInt32(8), "cmp");
+  BranchInst *BI2=ForCondBuilder.CreateCondBr(ICI1, ForBodyBB, ForEndBB);
+  
+  //Everything is okay with creation of "for.body" basic block! 
+  LoadInst *LI5=ForBodyBuilder.CreateLoad(ForBodyBuilder.getInt64Ty(), AI6);
+  Value *TI1=ForBodyBuilder.CreateTrunc(LI5, ForBodyBuilder.getInt8Ty(), "conv4");
+  Value *ZI4=ForBodyBuilder.CreateZExt(TI1, ForCondBuilder.getInt32Ty(), "conv5");
+  Value *And1=ForBodyBuilder.CreateAnd(ZI4, ForBodyBuilder.getInt32(1), "and");
+  Value *TI2=ForBodyBuilder.CreateTrunc(And1, ForBodyBuilder.getInt8Ty(), "conv6");
+  StoreInst *SI9=ForBodyBuilder.CreateStore(TI2, AI4);
+  LoadInst *LI6=ForBodyBuilder.CreateLoad(ForBodyBuilder.getInt8Ty(), AI1);
+  Value *ZI5=ForBodyBuilder.CreateZExt(LI6, ForCondBuilder.getInt32Ty(), "conv7");
+  Value *Ashr1=ForBodyBuilder.CreateAShr(ZI5, ForBodyBuilder.getInt32(1), "shr");
+  Value *TI3=ForBodyBuilder.CreateTrunc(Ashr1, ForBodyBuilder.getInt8Ty(), "conv8");
+  StoreInst *SI10=ForBodyBuilder.CreateStore(TI3, AI1);
+  LoadInst *LI7=ForBodyBuilder.CreateLoad(ForBodyBuilder.getInt64Ty(), AI6);
+  Value *Ashr2=ForBodyBuilder.CreateAShr(LI7, ForBodyBuilder.getInt64(1), "shr9");
+  StoreInst *SI11=ForBodyBuilder.CreateStore(Ashr2, AI6);
+  LoadInst *LI8=ForBodyBuilder.CreateLoad(ForBodyBuilder.getInt8Ty(), AI4);
+  Value *ZI6=ForBodyBuilder.CreateZExt(LI8, ForCondBuilder.getInt32Ty(), "conv10");
+  Value *And2=ForBodyBuilder.CreateAnd(ZI6, ForBodyBuilder.getInt32(1), "and11");
+  Value *ICI2=ForBodyBuilder.CreateICmp(llvm::CmpInst::ICMP_NE, And2, ForCondBuilder.getInt32(0), "tobool");
+  Value *ZI7=ForBodyBuilder.CreateZExt(ICI2, ForCondBuilder.getInt64Ty(), "");
+  Value *Select1=ForBodyBuilder.CreateSelect(ICI2, ForBodyBuilder.getInt32(40961), ForBodyBuilder.getInt32(0));
+  Value *Sext1=ForBodyBuilder.CreateSExt(Select1, ForBodyBuilder.getInt64Ty(), "conv12");
+  LoadInst *LI9=ForBodyBuilder.CreateLoad(ForBodyBuilder.getInt64Ty(), AI6);
+  Value *XOR2=ForBodyBuilder.CreateXor(LI9, Sext1, "xor13");
+  StoreInst *SI12=ForBodyBuilder.CreateStore(XOR2, AI6);
+  BranchInst *BI3=ForBodyBuilder.CreateBr(ForIncBB);
+
+  //Everything is okay with creation of "for.inc" basic block!
+  LoadInst *LI10=ForIncBuilder.CreateLoad(ForBodyBuilder.getInt8Ty(), AI3);
+  Value *Add1=ForIncBuilder.CreateAdd(LI10, ForIncBuilder.getInt8(1), "inc");
+  StoreInst *SI20=ForIncBuilder.CreateStore(Add1, AI3);
+  //We need to check this branch instruction again!
+  BranchInst *BI4=ForIncBuilder.CreateBr(ForCondBB);
+
+  //Last, we have to change a couple of instructions within "for.end" basic block!
+  IRBuilder<> ForEndBuilder(LIfinal);
+  LoadInst *LI11=ForEndBuilder.CreateLoad(B.getInt64Ty(), AI6);
+  Value* TI20=ForEndBuilder.CreateTrunc(LI11, B.getInt16Ty(), "conv14");
+  //ForEndBuilder.CreateRet(TI20);
+
+  LIfinal->replaceAllUsesWith(TI20);
+
+  //We will save this part of code! Just to have track of what we have created so far!
+  errs() << "-----------------------------------------\n";
+  for(BasicBlock &BBIT: *F){
+    errs() << BBIT.getName() << ":\n";
+    for(Instruction &IIT: BBIT){
+        errs() << IIT << "\n";
+    }
+  }
+  errs() << "-----------------------------------------\n";
 
   return true;
 }
@@ -1118,7 +1227,6 @@ static bool tryToRecognizeTableBasedCRC32(Instruction &I){
   LI=dyn_cast<LoadInst>(GEPI->getPrevNode());
   if(!LI)
     return false;
-
   
   LI=dyn_cast<LoadInst>(LI->getPrevNode());
   if(!LI)
@@ -1735,7 +1843,10 @@ static bool foldUnusualPatterns(Function &F, DominatorTree &DT,
   if(F.getName().str()=="reverse"){
     errs() << "We won't check this function!" << "\n";
     return false;
-  } 
+  }
+  bool crc_flag=tryToRecognizeCRC32(F.back().back());
+  if(crc_flag)
+    errs() << "CRC32 algorithm has been recognised!" << "\n"; 
   
   Module *M=F.getParent();
   for(Function &F: *M){
@@ -1757,14 +1868,14 @@ static bool foldUnusualPatterns(Function &F, DominatorTree &DT,
         MadeChange |= foldGuardedFunnelShift(I, DT);
         MadeChange |= tryToRecognizePopCount(I);
         bool flag1=tryToRecognizeTableBasedCRC32(I);
-        bool flag2=tryToRecognizeCRC32(I);
+        //bool flag2=tryToRecognizeCRC32(I);
         //bool flag2=false;
         MadeChange |= flag1;
         if(flag1)
           errs() << "Function we have created seems to work properly!\n";
 
-        if(flag2)
-          errs() << "CRC32 algorithm has been recognised!" << "\n";
+        //if(flag2)
+        //  errs() << "CRC32 algorithm has been recognised!" << "\n";
         MadeChange |= tryToFPToSat(I, TTI);
         //MadeChange |= tryToRecognizeTableBasedCttz(I);
         bool recognised=tryToRecognizeTableBasedCttz(I);
